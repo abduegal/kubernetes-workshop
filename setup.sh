58d1480a3cbae40c513d83f0eefd458514097708
abduegal <abdu.egal@gmail.com>
2019-12-27 17:40:46 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -23,4 +23,8 @@ mv ./kind /usr/bin/kind
 
 kind create cluster --config=cluster-config.yaml --quiet
 
+curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+
 newgrp docker 
