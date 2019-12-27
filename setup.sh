f0b2b27515cff7118c19a5608b80b3dda9a8b9da
abduegal <abdu.egal@gmail.com>
2019-12-27 17:23:34 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,4 +12,13 @@ nodes:
 - role: worker
 " > cluster-config.yaml
 
+sudo groupadd docker
+sudo usermod -aG docker $USER
+
+curl -Lo ./kind https://github.com/kubernetes-sigs/kind/releases/download/v0.6.1/kind-$(uname)-amd64
+chmod +x ./kind
+mv ./kind /usr/bin/kind
+
 kind create cluster --config=cluster-config.yaml --quiet
+
+newgrp docker 
